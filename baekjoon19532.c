b984f55ebaf49e18fcb2e0111209e83a695ffa7a
crayzing123 <kim315933@naver.com>
2023-05-25 17:37:35 +0900

    주석 넣은 백준 19532 수학은 비대면 강의입니다

--- baekjoon19532.c
+++ baekjoon19532.c
@@ -6,9 +6,9 @@ int main(void){
   int x = -999, y= -999;
   scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
 
-  for(x = -999; x<=999; x++){
+  for(x = -999; x<=999; x++){//다 집어넣기
     for(y = -999; y <= 999; y++){
-      if((a * x) + (b * y) == c && (d * x) + (e * y) == f){
+      if((a * x) + (b * y) == c && (d * x) + (e * y) == f){//조건 충족시 출력
         printf("%d %d", x, y);
         return 0;
       }
